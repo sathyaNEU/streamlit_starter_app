c269903110a80bffd47fd71a12ac390ba08f9f5d
sathyaNEU <144740003+sathyaNEU@users.noreply.github.com>
2024-11-16 23:43:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,10 @@ with st.expander('Data Visualisation'):
   # "bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g',color='species')
 
+with st.sidebar:
+  st.header('Input Features')
+  island = st.selectbox('Island',('Torgersen','Biscoe','Dream'))
+  gender = st.selectbox('Sex',('male','female'))
+  bill_length_mm = st.slider('Bill Length (mm)',32.1,59.6,43.9)
+
 
