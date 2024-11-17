00e501ee3fd3d24a4ff5b79958c4a88aecce04f2
sathyaNEU <144740003+sathyaNEU@users.noreply.github.com>
2024-11-16 23:22:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('😁 zBOT96 on Action!')
 
 st.info('My First Streamlit App, so far so good...')
 
-with st.expander('Data')
-  st.write('**Raw Data (dataframe) **')
+with st.expander('Data'):
+  st.write('**Raw Data (dataframe)**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
