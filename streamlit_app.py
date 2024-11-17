5e5a937ef346c5ac3cd90540cfa973f45922c400
sathyaNEU <144740003+sathyaNEU@users.noreply.github.com>
2024-11-17 15:51:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,9 @@ prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
  
 df_prediction_proba = pd.DataFrame(prediction_proba)
-df_prediction_proba.columns=target_mapper.values()
-# df_prediction_proba.rename(columns=rev_target_mapper)
+# df_prediction_proba.columns=target_mapper.values()
+rev_target_mapper
+df_prediction_proba.rename(columns=rev_target_mapper)
 df_prediction_proba
 
 
