45fd958143a9fe5d58e86516147a56d2b3fcf075
sathyaNEU <144740003+sathyaNEU@users.noreply.github.com>
2024-11-17 16:00:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,11 +77,12 @@ prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
  
 df_prediction_proba = pd.DataFrame(prediction_proba)
-# df_prediction_proba.columns=target_mapper.values()
 df_prediction_proba = df_prediction_proba.rename(columns={0: 'Adelie',
                                  1: 'Chinstrap',
                                  2: 'Gentoo'})
-df_prediction_proba
+st.subheader('Predicted Species')
+penguin_species = np.array(target_mapper.keys())
+st.success(penguin_species[prediction]) 
 
 
 
